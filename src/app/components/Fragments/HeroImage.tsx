8ec86da125888019360531a702d8f632e01e42b4
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-05-26 23:54:10 +0700

    Add HeroImage component to enhance page layout

---
+++ src/app/components/Fragments/HeroImage.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+interface HeroImageProps {
+  children?: React.ReactNode;
+  imgSrc: string;
+  imgAlt?: string;
+}
+
+export default function HeroImage({children, imgSrc, imgAlt}: HeroImageProps) {
+  return (
+    <div className="relative w-full h-[100vh] overflow-hidden">
+      <Image 
+        src={imgSrc}
+        alt={imgAlt || "Hero Image"}
+        width={1200}
+        height={600}
+        className="absolute inset-0 object-cover w-full h-full z-0"
+      />
+      <div className="container relative z-10 h-full">{children}</div>
+    </div>
+  )
+}
